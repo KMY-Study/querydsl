0a28dc26415d8d2fc8100937766f915b91ae08f3
KMY <kyobolife.ymk@gmail.com>
2023-12-18 16:58:02 +0900

    H2 db , JPA 설정 및 logging(log4j2, p6spy) 추가

--- src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -8,11 +8,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import com.study.querydsl.entity.QHello;
 
 @SpringBootTest
 @Transactional
+//@Commit <-
 class QuerydslApplicationTests {
 
     @Autowired
