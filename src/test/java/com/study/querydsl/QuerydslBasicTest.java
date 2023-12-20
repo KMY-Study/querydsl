fb748fc293ff96648c7e0f31a3f0073134b4e608
KMY <801sanae@gmail.com>
2023-12-21 00:16:49 +0900

    1. 특별한 Q Type Class를 이용해서 컴파일 시점 쿼리 문법 오류를 사전에 막는다. 2. 파라미터 바인딩을 해줌. (JPQL은 메서드 호출 런타임시점에 문법 오류 인지)

---
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,69 @@
+package com.study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.entity.Member;
+import com.study.querydsl.entity.QMember;
+import com.study.querydsl.entity.Team;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.assertj.core.api.Assertions;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+//    @Autowired
+    @PersistenceContext
+    EntityManager em;
+
+    @BeforeEach
+    public void before(){
+        Team teamA = new Team("TeamA");
+        Team teamB = new Team("TeamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 10, teamB);
+        Member member4 = new Member("member4", 20, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL(){
+        //member1
+        Member findMember = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl(){
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+}
